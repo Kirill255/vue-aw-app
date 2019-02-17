94fb4656b78ddffbb7f5424966ab294f0b2358e7
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-17 23:49:47 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <comp-header />
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+import compHeader from "./components/Header/Header.vue";
+
+export default {
+  components: {
+    compHeader
+  }
+};
+</script>
+
+
+<style>
+@import "./assets/style.css";
+</style>
